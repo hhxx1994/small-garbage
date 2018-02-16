ba4cdcfbea634a714d59e414ac3aa1664b5e8db8
hhxx1994 <2407660325@qq.com>
2018-02-16 14:10:46 +0800

    update: lianjia-scraler

---
+++ source/lianjia-scrawler/scrawl.py
@@ -0,0 +1,31 @@
+import thread
+
+import core
+import model
+import settings
+
+
+def get_communitylist(city):
+    res = []
+    for community in model.Community.select().where(model.Community.link.contains(city)):
+        res.append(community.title)
+    return res
+
+
+if __name__ == "__main__":
+    regionlist = settings.REGIONLIST  # only pinyin support
+    model.database_init()
+    for city in settings.CTTYS:
+        settings.CITY = city
+        core.initBASE_URL()
+        # thread.start_new_thread(core.GetHouseByRegionlist, (regionlist,))
+        # thread.start_new_thread(core.GetRentByRegionlist, (regionlist,))
+        # thread.start_new_thread(core.GetCommunityByRegionlist, (regionlist,))
+        # core.GetHouseByRegionlist(regionlist)
+        # core.GetRentByRegionlist(regionlist)
+        # core.GetCommunityByRegionlist(regionlist)  # Init,scrapy celllist and insert database; could run only 1st time
+        communitylist = get_communitylist(city)  # Read celllist from database
+        thread.start_new_thread(core.GetSellByCommunitylist, (communitylist,))
+        # core.GetSellByCommunitylist(communitylist)
+    while True:
+        pass
---
+++ source/lianjia-scrawler/settings.py
@@ -0,0 +1,9 @@
+DBENGINE = 'mysql'  # ENGINE OPTIONS: mysql, sqlite3, postgresql
+DBNAME = 'test'
+DBUSER = 'root'
+DBPASSWORD = '123456'
+DBHOST = '127.0.0.1'
+DBPORT = 3306
+CITY = ''  # only one, shanghai=sh shenzhen=sh......
+REGIONLIST = [u'luohu']  # only pinyin support
+CTTYS = ['sh', 'bj', 'sz', 'gz']
